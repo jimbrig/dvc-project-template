3c29fe53b69068e194c3c11104e4d89f0d3041b7
Jimmy Briggs <jimmy.briggs@pwc.com>
2022-12-01 20:13:14 -0500

    feat: add schemaspy dedicated docs directory with java jars and dockerfile

---
+++ docs/schemaspy/Generate-SchemaSpy.ps1
@@ -0,0 +1,13 @@
+java -jar ./etc/schemaspy-6.1.0.jar `
+	-t pgsql `
+	-dp ./etc/postgresql.jar `
+	-db postgres `
+	-host as-aurora-postgresql-provisioned-cluster-use1-dev.cluster-cbubbysvhwra.us-east-1.rds.amazonaws.com `
+	-port 5432 `
+	-u as_admin `
+	-p Pwcwelcome001 `
+	-o docs `
+    -s life `
+    -vizjs 
+    #-gv "C:\\Users\\jbriggs010\\scoop\\apps\\graphviz\\3.0.0"
+    
